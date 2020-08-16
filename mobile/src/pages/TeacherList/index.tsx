9cd58b5fdbc6200cf1d4813a0b8324101a752950
Riukkon <raphafm.rf@gmail.com>
2020-08-16 17:36:43 -0300

    Dia 5 - Parte 1

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matérias</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matérias"
+            placeholderTextColor="#c1bccc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o horário?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
